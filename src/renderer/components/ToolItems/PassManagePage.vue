d996491cdab42722b260d9dcf30ce3298763cf97
2lovecode <liu546hao@163.com>
2021-04-16 11:03:58 +0800

    pass

--- src/renderer/components/ToolItems/PassManagePage.vue
+++ src/renderer/components/ToolItems/PassManagePage.vue
@@ -1,6 +1,12 @@
 <template>
   <el-container>
-
+    <el-main>
+      <el-row>
+        <el-col>
+          
+        </el-col>
+      </el-row>
+    </el-main>
   </el-container>
 </template>
 <script>
