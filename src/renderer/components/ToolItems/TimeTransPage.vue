6446326a7972822f9458c52939b9267423351f19
2lovecode <liu546hao@163.com>
2021-06-01 18:21:52 +0800

    add-moment

--- src/renderer/components/ToolItems/TimeTransPage.vue
+++ src/renderer/components/ToolItems/TimeTransPage.vue
@@ -1,17 +1,30 @@
 <template>
   <el-row>
     <el-col>
-      good
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+      <el-button type="primary" @click="getM">立即创建</el-button>
     </el-col>
   </el-row>
 </template>
 <script>
+var moment = require('moment-timezone')
+
 export default {
   name: 'time-trans',
   data: () => {
-    return {}
+    return {
+      input: moment().tz("America/Los_Angeles").format()
+    }
   },
   methods: {
+    getM: function() {
+      var citys = moment.tz.countries()
+
+      citys.forEach((ele, idx) => {
+        console.log(ele)
+      })
+      // console.log(moment.tz.countries())
+    }
   }
 }
 </script>
