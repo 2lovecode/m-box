9d953464ab3a6f71c5a83fe65a31947c5ef4d79d
2lovecode <liu546hao@163.com>
2021-09-08 14:11:47 +0800

    game demo

--- src/renderer/components/Games/Demo.vue
+++ src/renderer/components/Games/Demo.vue
@@ -21,26 +21,35 @@ export default {
     var canvas = document.getElementById('renderCanvas')
     var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true})
     var createScene = function () {
-      const scene = new BABYLON.Scene(engine);
+      const scene = new BABYLON.Scene(engine)
 
-      const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
-      camera.attachControl(canvas, true);
+      const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0))
+      camera.attachControl(canvas, true)
 
-      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0))
 
-      const box = BABYLON.MeshBuilder.CreateBox("box", {});
+      const box = BABYLON.MeshBuilder.CreateBox("box", {})
       box.position.y = 0.5
 
-      const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
-      roof.scaling.x = 0.75;
-      roof.rotation.z = Math.PI / 2;
-      roof.position.y = 1.22;
+      const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3})
+      roof.scaling.x = 0.75
+      roof.rotation.z = Math.PI / 2
+      roof.position.y = 1.22
 
       // Ground
-      const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10}, scene);
-      const groundMat = new BABYLON.StandardMaterial("groundMat");
-      groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
-      ground.material = groundMat; //Place the material property of the ground
+      const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10}, scene)
+      const groundMat = new BABYLON.StandardMaterial("groundMat")
+      groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0)
+      ground.material = groundMat
+
+      const roofMat = new BABYLON.StandardMaterial("roofMat")
+      roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg", scene)
+
+      const boxMat = new BABYLON.StandardMaterial("boxMat")
+      boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png")
+
+      roof.material = roofMat
+      box.material = boxMat
 
       return scene
     }
